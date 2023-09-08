863d90f77097ef5f4f7bb9f113bb90982da5b2a8
Chungon <chungon.tse@gmail.com>
2023-09-08 10:15:07 -0400

    Update marks.sql

--- marks.sql
+++ marks.sql
@@ -1,4 +1,20 @@
+--marks 75
 SELECT name
 FROM students
 WHERE marks > 75
 ORDER BY RIGHT(name, 3), ID ASC
+--report
+SELECT name, grade, marks
+FROM 
+(
+    SELECT students.name, grades.grade, students.marks
+    FROM students
+    JOIN grades ON students.marks BETWEEN grades.min_mark AND grades.max_mark
+    WHERE grades.grade >= 8
+    UNION ALL
+    SELECT 'NULL' AS name, grades.grade, students.marks
+    FROM students
+    JOIN grades ON students.marks BETWEEN grades.min_mark AND grades.max_mark
+    WHERE grades.grade < 8
+) AS results
+ORDER BY grade DESC, name, marks ASC;
